93af870b4aa305557c6db2ec716e913ffb40e3a4
Marcos José <marcosjoseDV@hotmail.com>
2024-01-13 17:49:08 -0300

    Criando Projeto

---
+++ GuiaDeEstudo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GuiaDeEstudo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GuiaDeEstudo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GuiaDeEstudo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
